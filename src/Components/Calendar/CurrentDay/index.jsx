0ad4102c8edad20613c039869b5e554597348ba8
Evgeniya <evgeniyakirtoka@gmail.com>
2021-02-19 12:03:17 +0200

    Add CurrentDay styles

--- src/Components/Calendar/CurrentDay/index.jsx
+++ src/Components/Calendar/CurrentDay/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
+import classNames from 'classnames/bind';
+import styles from './CurrentDay.module.scss';
 
 const CurrentDay = (prors) => {
   const { date, className } = prors;
 
   return (
-    <div className={className}>
-      <p>{format(date, 'eeee')}</p>
-      <p>{format(date, 'd')}</p>
+    <div className={classNames(className, [styles.container])}>
+      <p className={styles.dayOfWeek}>{format(date, 'eeee')}</p>
+      <p className={styles.dayOfMonth}>{format(date, 'd')}</p>
     </div>
   );
 };
